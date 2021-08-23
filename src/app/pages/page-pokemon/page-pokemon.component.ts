e16d846bc848c1cce401b898c273879d575dcc5e
bruno.bozzetti <bruno.bozzetti@dbserver.com.br>
2021-08-23 20:28:36 -0300

    add loading variable to pokemon page

--- src/app/pages/page-pokemon/page-pokemon.component.ts
+++ src/app/pages/page-pokemon/page-pokemon.component.ts
@@ -8,6 +8,7 @@ import { ApiService } from 'src/app/services/api/api.service';
   styleUrls: ['./page-pokemon.component.scss'],
 })
 export class PagePokemonComponent implements OnInit {
+  public loading = false;
   public endpoint: string = '';
   public dadosPokemon: any = [];
   public pokemonId = '';
@@ -25,10 +26,12 @@ export class PagePokemonComponent implements OnInit {
   }
 
   public getPokemon(endpoint: string) {
+    this.loading = true;
     this.api.getEndpoint(endpoint).subscribe((data) => {
       this.dadosPokemon = data;
       console.log(this.dadosPokemon);
       this.convertPokemonId(this.dadosPokemon.id);
+      this.loading = false;
     });
   }
 
